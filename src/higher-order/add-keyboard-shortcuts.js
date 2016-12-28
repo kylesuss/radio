59acf125c481b65bfbc7370d3733cdc567fe31c2
Kyle Suss <susskyle@gmail.com>
2016-12-28 16:09:31 -0700

    feat: left, right arrow key shortcuts

--- src/higher-order/add-keyboard-shortcuts.js
+++ src/higher-order/add-keyboard-shortcuts.js
@@ -1,29 +1,59 @@
 import React, { Component } from 'react'
 import { store } from 'containers/root'
-import { togglePlayState } from 'actions/player'
+import { togglePlayState, playStation } from 'actions/player'
+import { findPrevStationBySlug, findNextStationBySlug } from 'selectors/station'
 
 const SPACE_KEY = 32
+const LEFT_ARROW_KEY = 37
+const RIGHT_ARROW_KEY = 39
 
 export default function (ComposedComponent) {
   return class extends Component {
     componentDidMount () {
-      window.addEventListener('keydown', this.handleKeyUp)
+      window.addEventListener('keydown', this.handleKeyDown)
     }
 
     componentWillUnMount () {
-      window.removeEventListener('keydown', this.handleKeyUp)
+      window.removeEventListener('keydown', this.handleKeyDown)
     }
 
-    handleKeyUp = (event) => {
+    // TODO: Is there a better way of doing this?
+    get storeState () {
+      return store.getState()
+    }
+
+    get stationList () {
+      return this.storeState.stations.items
+    }
+
+    get activeSlug () {
+      return this.storeState.player.activeStation
+    }
+
+    handleKeyDown = (event) => {
       switch (event.keyCode) {
         case SPACE_KEY:
           event.preventDefault() // Prevent scroll
           this.handleSpaceKey()
+          break
+        case LEFT_ARROW_KEY:
+          this.handleLeftArrowKey()
+          break
+        case RIGHT_ARROW_KEY:
+          this.handleRightArrowKey()
       }
     }
 
-    handleSpaceKey = () => {
-      store.dispatch(togglePlayState())
+    handleSpaceKey = () => store.dispatch(togglePlayState())
+
+    handleLeftArrowKey = () => {
+      const prevStation = findPrevStationBySlug(this.stationList, this.activeSlug)
+      store.dispatch(playStation(prevStation.slug))
+    }
+
+    handleRightArrowKey = () => {
+      const nextStation = findNextStationBySlug(this.stationList, this.activeSlug)
+      store.dispatch(playStation(nextStation.slug))
     }
 
     render () {

edb35d65375ae71165cf90e89d04e0d9523b76a9
Kyle Suss <susskyle@gmail.com>
2017-11-05 09:37:39 -0700

    fix: decode html entities in airtime response

--- src/models/live-info-airtime.js
+++ src/models/live-info-airtime.js
@@ -1,8 +1,10 @@
+import he from 'he'
+
 export default (response) => ({
   timezone: response.station.timezone,
   shows: {
     current: {
-      name: response.shows.current.name,
+      name: he.decode(response.shows.current.name),
       starts: response.shows.current.starts,
       ends: response.shows.current.ends
     }

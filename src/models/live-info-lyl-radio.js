8f14d879584db2191bef81477ba2d3fe854ff6ad
Kyle Suss <susskyle@gmail.com>
2017-11-27 16:57:42 -0700

    fix: lyl inactive status during the week

--- src/models/live-info-lyl-radio.js
+++ src/models/live-info-lyl-radio.js
@@ -3,7 +3,8 @@ import cleanLiveInfo from 'utils/clean-live-info'
 export default ({ body }) => {
   if (!body.current) { return null }
 
-  const isInactive = !!body.current.name.match(/Live streaming back on Monday/)
+  const isInactive = !!body.current.name.match(/Live streaming back on Monday/) ||
+                     !!body.current.name.match(/Live streaming starts at noon/)
   const status = cleanLiveInfo(body.current.name)
 
   return {

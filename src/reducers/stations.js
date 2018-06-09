7526d6a331d66a962d8ce6a57c8fd5c5366407fc
Kyle Suss <susskyle@gmail.com>
2018-06-09 17:39:44 -0600

    refactor: simplify stations reducer

--- src/reducers/stations.js
+++ src/reducers/stations.js
@@ -1,20 +1,19 @@
 import stationsFixtures from 'fixtures/stations'
 import sortBy from 'lodash/sortBy'
 
-const initialState = {
-  items: stationsFixtures.filter(station => station.isActive)
-}
+const activeStations = stationsFixtures.filter(station => station.isActive)
 
 const sortItems = (items, sortOptions) => sortBy(items, (item) => (
   item.name.toLowerCase()
 ))
 
+const initialState = {
+  items: sortItems(activeStations)
+}
+
 export default function (state = initialState, action = {}) {
   switch (action.type) {
     default:
-      return {
-        ...state,
-        items: sortItems(state.items)
-      }
+      return { ...state }
   }
 }

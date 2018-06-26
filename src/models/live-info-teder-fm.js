35c366d1d8d5f12d4ac583eef4f8eb67f7199ef8
Kyle Suss <susskyle@gmail.com>
2018-06-25 18:18:26 -0600

    fix: parse teder fm for offline message

--- src/models/live-info-teder-fm.js
+++ src/models/live-info-teder-fm.js
@@ -1,12 +1,24 @@
 import get from 'lodash/get'
-import { LIVE_INFO_CURRENT_KEY } from 'constants/live-info'
+import {
+  LIVE_INFO_CURRENT_KEY,
+  LIVE_INFO_STATUS_KEY,
+  LIVE_INFO_ACTIVE_STATUS,
+  LIVE_INFO_INACTIVE_STATUS
+} from 'constants/live-info'
 
 export default ({ text = '' }) => {
   const parser = new window.DOMParser()
   const dom = parser.parseFromString(text, 'text/html')
   const nameNode = dom.querySelector('.player .info .title')
+  const title = dom.querySelector('h1.title')
+  const isInactive = title.textContent === 'Off The Air'
 
   return {
-    [LIVE_INFO_CURRENT_KEY]: get(nameNode, 'textContent', '').trim()
+    [LIVE_INFO_STATUS_KEY]: isInactive
+      ? LIVE_INFO_INACTIVE_STATUS
+      : LIVE_INFO_ACTIVE_STATUS,
+    [LIVE_INFO_CURRENT_KEY]: isInactive
+      ? null
+      : get(nameNode, 'textContent', '').trim()
   }
 }

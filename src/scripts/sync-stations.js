82f567987e1a3bb8afdf06c7aaf31821e6dc2f45
Kyle Suss <susskyle@gmail.com>
2018-06-30 13:04:34 -0600

    chore: add script to sync stations to dynamo db

---
+++ src/scripts/sync-stations.js
@@ -0,0 +1,33 @@
+import aws from 'aws-sdk'
+import stations from '../fixtures/stations'
+
+const TABLE_NAME = 'stations'
+const REGION = 'us-east-1'
+const REQUEST_ITEMS = 'RequestItems'
+
+const buildWriteItemParams = (stations) => (
+  stations.reduce((accumulator, station) => ({
+    ...accumulator,
+    [REQUEST_ITEMS]: {
+      [TABLE_NAME]: accumulator[REQUEST_ITEMS][TABLE_NAME].concat({
+        PutRequest: { Item: { ...station } }
+      })
+    }
+  }), { [REQUEST_ITEMS]: { [TABLE_NAME]: [] } })
+)
+
+const dynamoDB = new aws.DynamoDB.DocumentClient({
+  region: REGION
+})
+
+const writeParams = buildWriteItemParams(stations)
+
+dynamoDB.batchWrite(buildWriteItemParams(stations), (err, data) => {
+  if (err) {
+    console.log(`ERROR: ${err}`)
+  }
+
+  if (data) {
+    console.log(`SUCCESS: ${JSON.stringify(data, null, 2)}`)
+  }
+})

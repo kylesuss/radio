c04d9f894e4d21af1858dcc8d0134c7572bf5322
Kyle Suss <susskyle@gmail.com>
2019-02-20 16:57:19 -0700

    fix: add jest testUrl to fix localStorage issues

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   modulePaths: ['<rootDir>/src'],
   setupFiles: ['jest-localstorage-mock'],
   setupTestFrameworkScriptFile: '<rootDir>/src/jest/setup-framework.js',
+  testURL: 'http://localhost/',
   verbose: true,
 }
